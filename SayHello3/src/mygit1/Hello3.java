97c4c36600d1f8351b9094ec2583cec618076141
JoniQFZ <JoniQFZ@10.1.1.3>
2016-01-16 19:00:28 +1100

    add git1

--- SayHello3/src/mygit1/Hello3.java
+++ SayHello3/src/mygit1/Hello3.java
@@ -4,7 +4,7 @@ public class Hello3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello 3!");
+		System.out.println("Hello Git1 3!");
 	}
 
 }
