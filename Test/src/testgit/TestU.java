25e25433b2d2e62f0dd40edbdce64be3cccbf8c0
JoniQFZ <JoniQFZ@10.1.1.3>
2016-01-16 22:38:12 +1100

    commit Test project to local git.

---
+++ Test/src/testgit/TestU.java
@@ -0,0 +1,14 @@
+package testgit;
+
+public class TestU {
+
+	public TestU() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
